bf0eea26cd2d9ce49a90db8d7fd890347c070d9d
Jalaj Punn <jalaj.punn@salesforce.com>
2019-08-29 19:29:29 -0700

    Added player + move class

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,15 @@
+class Player:
+
+	def __init__(self, name, symbol):
+		self.name = name
+		self.symbol = symbol 
+
+
+class Move:
+
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+
 
 
